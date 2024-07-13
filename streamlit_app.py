a7196b512f3500e9758498e83444c851677e9685
tom-baz <65848772+tom-baz@users.noreply.github.com>
2024-07-13 21:21:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,6 @@ import random
 import io
 
 
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
